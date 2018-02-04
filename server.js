7b314698686174f9ec06e2217e38f28b978637b8
danale <danale@danales-MacBook-Pro.local>
2018-02-04 17:08:56 -0500

    configured server.js for production

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+// const express = require('express');
+// const path = require('path');
+// const port = process.env.PORT || 8080;
+// const app = express();
+//
+// app.use(express.static(__dirname));
+//
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'index.html'))
+// });
+//
+// app.listen(port);
+// console.log('Server started');
+
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+// const port = process.env.port || 8080;
 const app = express();
-
-app.use(express.static(__dirname));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
+app.listen(process.env.PORT || 3000 ,function(){
+    console.log("Server started "+process.env.PORT);
 });
-
-app.listen(port);
-console.log('Server started');
+app.use(express.static(__dirname));
+app.get('*' , (req, res) => {
+ res.sendFile(path.resolve(__dirname,'index.html'))
+});
