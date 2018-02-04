46906d5519fe9e4d4b3eb35e658e373033351cd9
danale <danale@danales-MacBook-Pro.local>
2018-02-04 15:21:24 -0500

    configured server.js for production

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'bundle.js'))
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 });
 
