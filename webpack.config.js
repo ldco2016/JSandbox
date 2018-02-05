06e5657f9d4cb8d04eeb51f11b76d4663238893d
danale <danale@danales-MacBook-Pro.local>
2018-02-04 20:20:57 -0500

    configured package.json for production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'src'),
+    path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
